d5f01950d2a3be53b2bc5d6d8a31510717b9caa6
Kent Fredric <kentfredric@gmail.com>
2014-07-06 12:12:56 +1200

    Build results of 63048c2 (on master)

---
+++ lib/MooX/LSub.pm
@@ -0,0 +1,45 @@
+use 5.008;    # utf8
+use strict;
+use warnings;
+use utf8;
+
+package MooX::LSub;
+
+our $VERSION = '0.001000';
+
+# ABSTRACT:
+
+our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
+
+use Moo;
+
+no Moo;
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+MooX::LSub - # AUTHORITY
+
+=head1 VERSION
+
+version 0.001000
+
+=head1 AUTHOR
+
+Kent Fredric <kentfredric@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Kent Fredric <kentfredric@gmail.com>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
