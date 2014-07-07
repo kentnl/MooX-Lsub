534d9715482c513783a178c5d3815c95eb69f7f9
Kent Fredric <kentfredric@gmail.com>
2014-07-07 16:01:03 +1200

    Minor Tweaks and criticisms

--- lib/MooX/Lsub.pm
+++ lib/MooX/Lsub.pm
@@ -40,39 +40,44 @@ But switching things to Moo means I usually have to get much uglier, and repeat 
 
 So this module exists as a compromise.
 
-Additionally, I always forgot to declare C<use Moo 1.000008> which was the first version of C<Moo> where C<< builder => sub >> worked, and I would invariably get silly test failures in smokers as a consequence.
+Additionally, I always forgot to declare C<use Moo 1.000008> which was the first version of C<Moo> where
+C<< builder => sub >> worked, and I would invariably get silly test failures in smokers as a consequence.
 
 This module avoids such problem entirely, and is tested to work with C<Moo 0.009001>.
 
 =cut
 
-use Eval::Closure;
+use Eval::Closure qw(eval_closure);
+use Carp qw(croak);
 
+## no critic (TestingAndDebugging::ProhibitNoStrict)
 sub _get_sub {
-  my ( $class, $target, $subname ) = @_;
+  my ( undef, $target, $subname ) = @_;
   no strict 'refs';
-  return \&{ $target . '::' . $subname };
+  return \&{ $target . q[::] . $subname };
 }
 
 sub _set_sub {
-  my ( $class, $target, $subname, $code ) = @_;
+  my ( undef, $target, $subname, $code ) = @_;
   no strict 'refs';
-  *{ $target . '::' . $subname } = $code;
+  *{ $target . q[::] . $subname } = $code;
+  return;
 }
-
+## use critic
+#
 sub import {
   my ( $class, @args ) = @_;
   my $target = caller;
   my $has = $class->_get_sub( $target, 'has' );
 
-  die "No 'has' method in $target. Did you forget to import Moo(se)?" if not $has;
+  croak "No 'has' method in $target. Did you forget to import Moo(se)?" if not $has;
 
   my $lsub_code = $class->_make_lsub(
     {
       target  => $target,
       has     => $has,
       options => \@args,
-    }
+    },
   );
 
   $class->_set_sub( $target, 'lsub', $lsub_code );
@@ -80,33 +85,50 @@ sub import {
   return;
 }
 
-sub _make_lsub {
+sub _make_lsub_code {
   my ( $class, $options ) = @_;
-
+  ## no critic (ValuesAndExpressions::RequireInterpolationOfMetachars)
   my $nl   = qq[\n];
   my $code = 'sub($$) {' . $nl;
   $code .= q[ package ] . $class . q[; ] . $nl;
   $code .= q[ my ( $subname, $sub , @extras ) = @_; ] . $nl;
   $code .= q[ if ( @extras ) { ] . $nl;
-  $code .= q[   die "Too many arguments to 'lsub'. Did you misplace a ';'?"; ] . $nl;
+  $code .= q[   croak "Too many arguments to 'lsub'. Did you misplace a ';'?"; ] . $nl;
   $code .= q[ } ] . $nl;
   $code .= q[ if ( not defined $subname or not length $subname or ref $subname ) { ] . $nl;
-  $code .= q[   die "Subname must be defined + length + not a ref"; ] . $nl;
+  $code .= q[   croak "Subname must be defined + length + not a ref"; ] . $nl;
   $code .= q[ } ] . $nl;
   $code .= q[ if ( not 'CODE' eq ref $sub ) { ] . $nl;
-  $code .= q[   die "Sub must be a CODE ref"; ] . $nl;
+  $code .= q[   croak "Sub must be a CODE ref"; ] . $nl;
   $code .= q[ } ] . $nl;
   $code .= q[ $class->_set_sub($target, "_build_" . $subname , $sub ); ] . $nl;
   $code .= q[ package ] . $options->{'target'} . q[; ] . $nl;
-  $code .= q[ return $has->( $subname, is => 'ro', lazy => 1, builder => '_build_' . $subname ); ] . $nl;
+  $code .= q[ return $has->( ] . $nl;
+  $code .= q[   $subname, ] . $nl;
+  $code .= q[   ( ] . $nl;
+  $code .= q[     is => 'ro', ] . $nl;
+  $code .= q[     lazy => 1, ] . $nl;
+  $code .= q[     builder => '_build_' . $subname, ] . $nl;
+  $code .= q[   ) ] . $nl;
+  $code .= q[ ); ] . $nl;
   $code .= q[}] . $nl;
+  ## use critic
+  return $code;
+}
+
+sub _make_lsub {
+  my ( $class, $options ) = @_;
+
+  my $code = $class->_make_lsub_code($options);
 
   my $sub = eval_closure(
     source      => $code,
     environment => {
+      ## no critic (ValuesAndExpressions::RequireInterpolationOfMetachars)
       '$class'  => \$class,
       '$has'    => \$options->{'has'},
       '$target' => \$options->{'target'},
+      ## use critic
     },
   );
   return $sub;
--- maint/perlcritic.rc.gen.pl
+++ maint/perlcritic.rc.gen.pl
@@ -26,7 +26,8 @@ for my $wordlist (@stopwords) {
 #$bundle->add_or_append_policy_field(
 #  'Subroutines::ProhibitCallsToUndeclaredSubs' => ( 'exempt_subs' => 'String::Formatter::str_rf' ), );
 
-#$bundle->remove_policy('ErrorHandling::RequireUseOfExceptions');
+$bundle->remove_policy('ErrorHandling::RequireUseOfExceptions');
+
 #$bundle->remove_policy('CodeLayout::RequireUseUTF8');
 #$bundle->remove_policy('ErrorHandling::RequireCarping');
 #$bundle->remove_policy('NamingConventions::Capitalization');
