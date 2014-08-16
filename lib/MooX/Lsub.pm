274f2c6a3ce93565b19546ea6ffd96a71402ed29
Kent Fredric <kentfredric@gmail.com>
2014-08-16 12:40:02 +1200

    Build results of b4d85a4 (on master)

--- lib/MooX/Lsub.pm
+++ lib/MooX/Lsub.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package MooX::Lsub;
 
-our $VERSION = '0.001003';
+our $VERSION = '0.001004';
 
 # ABSTRACT: Very shorthand syntax for bulk lazy builders
 
@@ -121,7 +121,7 @@ MooX::Lsub - Very shorthand syntax for bulk lazy builders
 
 =head1 VERSION
 
-version 0.001003
+version 0.001004
 
 =head1 SYNOPSIS
 
